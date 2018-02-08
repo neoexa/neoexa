6ddc4706fff32c282bf5fee3af0dc6da8378069a
mmdbareche <mmdbareche@gmail.com>
2018-02-08 22:39:44 +0100

    Source module

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,16 @@ module.exports = {
   siteMetadata: {
     title: 'neoexa',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sass',
+    {
+      resolve:'gatsby-source-filesystem',
+      options: {
+        path: '${__dirname}/src/posts',
+        name: 'posts',
+      }
+    },
+    'gatsby-transformer-remark'
+  ],
 };
